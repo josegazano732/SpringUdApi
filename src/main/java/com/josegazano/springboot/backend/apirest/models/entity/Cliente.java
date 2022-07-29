507f1e56164df343680838d62b82c822808f83fa
JLG <josegazano7@outlook.com>
2022-07-29 12:44:32 -0300

    Se soluciono corsorigin en resourceserver. JLG

--- src/main/java/com/josegazano/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/josegazano/springboot/backend/apirest/models/entity/Cliente.java
@@ -17,7 +17,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
