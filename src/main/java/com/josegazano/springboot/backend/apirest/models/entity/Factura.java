00a5b1bf38e4a6291542233f3ae2a5b1e7002131
JLG <josegazano7@outlook.com>
2022-08-10 13:43:58 -0300

    JLG

--- src/main/java/com/josegazano/springboot/backend/apirest/models/entity/Factura.java
+++ src/main/java/com/josegazano/springboot/backend/apirest/models/entity/Factura.java
@@ -5,9 +5,12 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -32,8 +35,14 @@ public class Factura implements Serializable{
     @Temporal(TemporalType.DATE)
     private Date createAt;
     
+    @ManyToOne(fetch = FetchType.LAZY)
     private Cliente cliente;
-
+    
+    @PrePersist
+    public void prePersist(){
+    this.createAt = new Date();
+    }
+    
     public Long getId() {
         return id;
     }
